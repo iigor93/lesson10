dc9d3bf0ab5379f3aa88f15b273cec4e5d79c6e2
Igor <iigor93@gmai.com>
2022-03-01 19:33:27 +0300

    view for 'skill/skill_name'

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import json
 
 app = Flask(__name__)
 
+# загружаем данные из файла
 with open('candidates.json', 'r', encoding='utf-8') as file:
     json_data = json.load(file)
 
 
 @app.route('/')
 def main():
+    """вывод всех кандидатов"""
     string_to_return = '<pre><br>'
     for items in json_data:
         string_to_return += 'Имя кандидата - ' + items.get('name') + '<br>'
@@ -18,8 +20,10 @@ def main():
     string_to_return += '</pre>'
     return string_to_return
 
+
 @app.route('/candidate/<int:uid>/')
 def candidate_data(uid):
+    """Вывод кандидата по номеру"""
     photo_link = 'https://avatars.mds.yandex.net/i?id=81113664ac71b0cc444a0dc2ea53dffc-5616093-images-thumbs&n=13&exp=1'
 
     for items in json_data:
@@ -36,6 +40,18 @@ def candidate_data(uid):
     return 'Нет такого кандидата'
 
 
-@app.route('/skill/<skill_name>')
+@app.route('/skill/<skill_name>/')
 def skills_view(skill_name):
-    pass
+    """Вывод кандидатов по навыкам"""
+    string_to_return = '<pre>'
+    for items in json_data:
+        for skill_item in items.get('skills').split(','):
+            if skill_name.strip().lower() == skill_item.strip().lower():
+                string_to_return += 'Имя кандидата - ' + items.get('name') + '<br>'
+                string_to_return += 'Позиция кандидата - ' + items.get('position') + '<br>'
+                string_to_return += 'Навыки - ' + items.get('skills') + '<br>'
+                string_to_return += '<br>'
+
+    if len(string_to_return) > 20:
+        return string_to_return
+    return 'Вашего запроса нет в списке'
