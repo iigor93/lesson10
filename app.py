1e4d9fb4b9d69a15f5c2f02f3875c6f4714e2260
Igor <iigor93@gmai.com>
2022-03-01 19:09:35 +0300

    view for 'candidate/uid'

--- app.py
+++ app.py
@@ -17,3 +17,25 @@ def main():
         string_to_return += '<br>'
     string_to_return += '</pre>'
     return string_to_return
+
+@app.route('/candidate/<int:uid>/')
+def candidate_data(uid):
+    photo_link = 'https://avatars.mds.yandex.net/i?id=81113664ac71b0cc444a0dc2ea53dffc-5616093-images-thumbs&n=13&exp=1'
+
+    for items in json_data:
+        if items.get('id') == uid:
+            string_to_return = f'<img src = "{photo_link} width = 200 height = 100"><br><pre>'
+            string_to_return += 'Имя кандидата - ' + items.get('name') + '<br>'
+            string_to_return += 'Позиция кандидата - ' + items.get('position') + '<br>'
+            string_to_return += 'Навыки - ' + items.get('skills') + '<br>'
+            string_to_return += '<br>'
+            string_to_return += '</pre>'
+
+            return string_to_return
+
+    return 'Нет такого кандидата'
+
+
+@app.route('/skill/<skill_name>')
+def skills_view(skill_name):
+    pass
